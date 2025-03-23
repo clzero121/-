4cfbebc44805b134b2e9b1efe839abb4f6f8c5bb
clzero121 <clzero121@gmail.com>
2025-03-23 13:55:50 +0900

    find lcm.cpp
    주석 추가

--- find lcm.cpp
+++ find lcm.cpp
@@ -5,15 +5,18 @@ using namespace std;
 int n;
 int arr[10];
 
+// 최대공약수 구하기
 int gcd(int a, int b) {
     if (b==0) return a;
     return gcd(b, a%b);
 }
 
+// 최소공배수 구하기
 int lcm(int a, int b) {
     return a*b / gcd(a, b);
 }
 
+// 리스트의 수를 하나씩 꺼내서 여러 수의 최소공배수 구하기
 int list_lcm(int a[], int n) {
     if (n==1) return a[0];
     return lcm(a[n-1], list_lcm(a, n-1));
@@ -25,7 +28,6 @@ int main() {
         cin >> arr[i];
     }
 
-    // Please write your code here.
     cout << list_lcm(arr, n);
 
     return 0;
